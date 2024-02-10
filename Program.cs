39f109385ac07ea6b759d6f6d3afc30c0c7ffde3
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 13:19:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PetlaDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczby całkowite z przedziału od 1 do 20 podniesione do kwadratu:");
+
+            int number = 1;
+            do
+            {
+                Console.WriteLine(Math.Pow(number, 2));
+            } while (number++ <= 20);
+        }
+    }
+}
